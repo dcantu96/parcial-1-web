9571278b08360cfd9626a473f31503ed1f2b56e2
David Cantu Martinez <davidcantu966@gmail.com>
2019-02-20 23:31:45 -0600

    Fixed search route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post :search, to: "home#show", as: :search
 
   get 'show', to: 'home#show', as: :show
+  get 'search', to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
